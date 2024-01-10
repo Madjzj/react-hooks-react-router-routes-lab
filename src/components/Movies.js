cdb8b0758d571b157bb5b2113a7371cc7f1413be
JakobJohnson <madjzj@gmail.com>
2024-01-10 15:02:05 -0800

    Completed Lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return  <div>
+            <h1>Movies Page</h1>
+            {
+              movies.map( (movie,index)=> { 
+                return  <div key={index}>
+                          <div>Name: {movie.title}</div>
+                          <div>Time: {movie.time}</div>
+                          <ul>
+                          {movie.genres.map((genre,index2)=>
+                            {return <li key={index2}>{genre}</li>}
+                          )}
+                        </ul> 
+                        </div>
+              })
+            }
+          </div>;
 }
 
 export default Movies;
